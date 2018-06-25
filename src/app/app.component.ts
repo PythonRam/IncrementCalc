f2a5668e5b0bd36d0bf31474e344c02594f27d76
pythonRam <python.hithesh@gmail.com>
2018-06-25 17:03:18 +0530

    removed moment.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
-import * as _moment from 'moment';
+
 import { FormControl } from '@angular/forms';
-import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-const moment = _moment;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [
-    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
-    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
-  ],
 })
 export class AppComponent {
-  today = moment();
-  date_of_joining = new FormControl(moment([this.today.year(), this.today.month(), this.today.date()]));
+  date_of_joining = new FormControl();
+  days_before_increment = new FormControl(365);
   leaves = [1, 2, 3, 4, 5];
 }
