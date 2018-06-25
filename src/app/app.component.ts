c2d28b2b78d9c1c4ba5cf287d10f9315d8ec0d0b
pythonRam <python.hithesh@gmail.com>
2018-06-25 21:44:27 +0530

    worKING

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,26 @@ import { FormControl } from '@angular/forms';
 export class AppComponent {
   date_of_joining = new FormControl();
   days_before_increment = new FormControl(365);
+  date_of_increment: Date;
   leaves = [];
   minDate: Date;
   maxDate: Date;
 
+  computeDate() {
+    let days = 0;
+    this.leaves.forEach(l => {
+      days += l.days * 24 * 60 * 60 * 1000;
+    });
+    const extra = this.days_before_increment.value * 24 * 60 * 60 * 1000;
+    this.date_of_increment = (new Date(((new Date(this.date_of_joining.value).valueOf()) + days +  extra)));
+  }
   days(e, i) {
-    console.log(e, i);
+    this.leaves[i]['days'] = e;
+    this.computeDate();
   }
   delete(i) {
     this.leaves.splice(i, 1);
+    this.computeDate();
   }
 
   addLeave() {
@@ -27,5 +38,6 @@ export class AppComponent {
   computeMinMax() {
     this.minDate = new Date(this.date_of_joining.value);
     this.maxDate = new Date(new Date(this.date_of_joining.value).valueOf() + this.days_before_increment.value * 24 * 60 * 60 * 1000);
+    this.computeDate();
   }
 }
--- src/app/components/leave/leave.component.ts
+++ src/app/components/leave/leave.component.ts
@@ -12,6 +12,7 @@ export class LeaveComponent implements OnInit {
   @Input('maxDate') maxDate: Date;
   @Output() delete: EventEmitter<any> = new EventEmitter();
   @Output() days: EventEmitter<any> = new EventEmitter();
+  leaves = 0;
   leave_start = new FormControl();
   leave_end = new FormControl();
   constructor() {
@@ -19,6 +20,12 @@ export class LeaveComponent implements OnInit {
   }
   DeleteLeave() {
     this.delete.emit();
+  }
+  computeDays() {
+    if (this.leave_start.valid && this.leave_end.valid && this.leave_end.value >= this.leave_start.value) {
+      this.leaves = ((this.leave_end.value - this.leave_start.value) / (24 * 60 * 60 * 1000)) + 1;
+      this.days.emit(this.leaves);
+    }
   }
 
   ngOnInit() {
